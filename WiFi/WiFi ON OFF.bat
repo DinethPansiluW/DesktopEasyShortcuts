743b0441a2ca58855047d5321c5c43f6325ccc93
Dineth Pansilu <dinethpansilu2004@gmail.com>
2025-05-07 21:03:56 +0530

    Delete WiFi directory

--- WiFi/WiFi ON OFF.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-netsh interface show interface name="Wi-Fi" | findstr /C:"Connected" >nul
-if %errorlevel%==0 (
-    start "" "WiFi OFF.lnk"
-) else (
-    start "" "WiFi ON.lnk"
-)
-exit
