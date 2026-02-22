2b4d8d09b4a592ae92a753fd01d743c0bcd906ed
Dineth Pansilu <dinethpansilu2004@gmail.com>
2026-02-23 01:38:17 +0530

    updated

--- Wi-Fi ON OFF/Run.bat
+++ Wi-Fi ON OFF/Run.bat
@@ -1,2 +1,10 @@
 @echo off
-call "D:\- Scripts\Wi-Fi ON OFF\WiFiNameChange1.bat"
+set "WiFiName=Dialog 4G 421"
+
+netsh interface show interface name="Wi-Fi" | findstr /C:"Connected" >nul
+if %errorlevel%==0 (
+    netsh wlan disconnect
+) else (
+    netsh wlan connect name="%WiFiName%"
+)
+exit
